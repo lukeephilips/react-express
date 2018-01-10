90679a39dcde72c77df033c6fb5f5116aa52e528
Luke <luke.e.philips@gmail.com>
2018-01-09 16:18:40 -0800

    all that over that?

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const path = require('path');
 
 const config = {
-  resolve: {
-    modules: [
-      path.resolve('./lib'),
-      path.resolve('./node_modules'),
-    ]
-  },
-  entry: ['babel-polyfill', './lib/renderers/dom.js'],
+  // resolve: {
+  //   modules: [
+  //     path.resolve('./lib'),
+  //     path.resolve('./node_modules'),
+  //   ]
+  // },
+  entry: ['babel-polyfill', './lib/components/Index.js'],
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
